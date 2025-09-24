c4d2791d5e83f0c8498d796955b98135141a1fba
kalebsnhu <120768449+kalebsnhu@users.noreply.github.com>
2025-09-24 13:59:37 -0500

    Update DataItem.java

--- enhancements/CS360/ModuleThreeAssignmentKalebWard/app/src/main/java/com/example/modulethreeassignmentkalebward/DataItem.java
+++ enhancements/CS360/ModuleThreeAssignmentKalebWard/app/src/main/java/com/example/modulethreeassignmentkalebward/DataItem.java
@@ -1,5 +1,10 @@
 package com.example.modulethreeassignmentkalebward;
 
+/*
+ * Class: DataItem
+ * Description: This class is for the actual inventory data items, it allows for various variables assigned
+*/
+
 public class DataItem {
     private long id = -1; 
     private String name;
@@ -21,42 +26,99 @@ public class DataItem {
         this.expirationDate = expirationDate != null ? expirationDate : "";
     }
 
-    
+    /*
+    * getId
+    * Description: Receives ID of item
+    */
+
     public long getId() {
         return id;
     }
+
+    /*
+    * setId
+    * @params()
+        *long id
+    * Description: Sets the ID of the item
+    */
 
     public void setId(long id) {
         this.id = id;
     }
 
+    /*
+    * getName
+    * Description: Receives name of the item
+    */
+
     public String getName() {
         return name;
     }
+
+    /*
+    * setName
+    * @params()
+        *string name
+    * Description: Sets the name of the item
+    */
 
     public void setName(String name) {
         this.name = name;
     }
 
+    /*
+    * getQuantity
+    * Description: Receives quantity of the item
+    */
+
     public String getQuantity() {
         return quantity;
     }
+
+    /*
+    * setQuantity
+    * @params()
+        *string quantity
+    * Description: Sets the quantity of the item
+    */
 
     public void setQuantity(String quantity) {
         this.quantity = quantity;
     }
 
+    /*
+    * getDate
+    * Description: Receives posted date of the item
+    */
+
     public String getDate() {
         return date;
     }
+
+    /*
+    * setDate
+    * @params()
+        *string date
+    * Description: Sets the posted date of the item
+    */
 
     public void setDate(String date) {
         this.date = date;
     }
 
+    /*
+    * getExpirationDate
+    * Description: Receives experation date of the item
+    */
+
     public String getExpirationDate() {
         return expirationDate;
     }
+
+    /*
+    * setExpirationDate
+    * Description: sets experation date of the item
+    */
 
     public void setExpirationDate(String expirationDate) {
         this.expirationDate = expirationDate != null ? expirationDate : "";
